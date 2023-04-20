79b3cce86b13a960fd88559fcc929dc2bcec2d16
Luis-Mocha <luismocha97@gmail.com>
2023-04-20 11:49:45 +0200

    creazione file / setting vue

---
+++ assets/javascript/main.js
@@ -0,0 +1,17 @@
+const {createApp} = Vue
+
+createApp({
+    data() {
+        return {
+            // variabili Vue3
+
+            
+        }
+    },
+
+    methods: {
+        // funzioni Vue3
+        
+        
+    }
+}).mount('#app')
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Esercizio "vue-todolist"
