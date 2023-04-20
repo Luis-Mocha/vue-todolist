5f3337b4ce7b31c069e7fc18694e220778032430
Luis-Mocha <luismocha97@gmail.com>
2023-04-20 11:59:39 +0200

    oggetto todos

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,6 +4,20 @@ createApp({
     data() {
         return {
             // variabili Vue3
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
 
             
         }
